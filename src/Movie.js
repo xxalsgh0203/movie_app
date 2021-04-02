91a501fb80f296040f81b4d57258d0c5050d077b
xxalsgh0203 <music4537@naver.com>
2021-04-02 15:39:00 +0900

    revised css

--- src/Movie.js
+++ src/Movie.js
@@ -12,9 +12,7 @@ function Movie({id, year, title, summary, poster, genres}){
             <h5 className="movie__year">{year}</h5>
             <ul className="genres">
             {genres.map((genre, index) => (
-                <li key={index} className="genres__genre">
-                {genre}
-                </li>
+                <li key={index} className="movie__genres">{genre+" "}</li>
             ))}
             </ul>
             <p className="movie__summary">{summary.slice(0, 180)}...</p>
