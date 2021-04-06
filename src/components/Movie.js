015f3abb38ea5eaf64cc0834429a794766737c61
xxalsgh0203 <music4537@naver.com>
2021-04-06 11:59:36 +0900

    all work done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 // state 가 필요하지 않으므로 function 
 // state 가 필요 없을 경우에는 class component 가 될 필요가 없다
 function Movie({id, year, title, summary, poster, genres}){
-    return <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
-            {genres.map((genre, index) => (
-                <li key={index} className="movie__genres">{genre+" "}</li>
-            ))}
-            </ul>
-            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+    return (
+        <div className="movie">
+            <Link to={{
+                pathname: `/movie/${id}`,
+                state: {
+                    year:year,
+                    title:title,
+                    summary:summary,
+                    poster:poster,
+                    genres:genres
+                }
+            }}>
+                <div className="movie">
+                    <img src={poster} alt={title} title={title} />
+                    <div className="movie__data">
+                        <h3 className="movie__title">{title}</h3>
+                        <h5 className="movie__year">{year}</h5>
+                        <ul className="genres">
+                        {genres.map((genre, index) => (
+                            <li key={index} className="movie__genres">{genre+" "}</li>
+                        ))}
+                        </ul>
+                        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+                    </div>
+                </div>
+            </Link>
         </div>
-    </div>
+    );
 }
 
 Movie.propTypes = {
